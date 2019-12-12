3551445879eb886572da7d6553f2b3ae591f7c78
Space School <space.school.kg@gmail.com>
2019-12-12 18:16:29 +0600

    check scroll

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "212610a02238a80a9bb7";
+/******/ 	var hotCurrentHash = "82ad2e94ba401fde5977";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- .next/static/webpack/styles.212610a02238a80a9bb7.hot-update.js
+++ .next/static/webpack/styles.212610a02238a80a9bb7.hot-update.js
@@ -1,4 +1,4 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["styles"],{
+webpackHotUpdate("styles",{
 
 /***/ "./scss/app.scss":
 /*!***********************!*\
@@ -37,5 +37,5 @@
 
 /***/ })
 
-}]);
-//# sourceMappingURL=styles.js.map
+})
+//# sourceMappingURL=styles.212610a02238a80a9bb7.hot-update.js.map
