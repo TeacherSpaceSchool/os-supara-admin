62c56c38abfc60635d781cc1ac751bbe84c85271
Space School <space.school.kg@gmail.com>
2020-04-08 23:23:27 +0600

    check bug

--- pages/agentroute/[id].js
+++ pages/agentroute/[id].js
@@ -97,8 +97,8 @@ const AgentRoute = React.memo((props) => {
                 else if (selectType == 'Свободные')
                     allClient=district.client.filter(client=>!clients[dayWeek].includes(client._id))
                 else if (selectType == 'Выбраные') {
-                    allClient = clients[dayWeek].filter(client=>client._id)
                     allClient = clients[dayWeek].map(client=>district.client.find(client1=>client1._id===client))
+                    allClient = clients[dayWeek].filter(client=>client._id)
                 }
                 let filtredClient = [...allClient]
                 if(search.length>0)
