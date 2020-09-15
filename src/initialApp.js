dfc2e79f5841bc401c1b3e444793d6dd349da96a
Space School <space.school.kg@gmail.com>
2020-09-15 19:03:34 +0600

    check

--- src/initialApp.js
+++ src/initialApp.js
@@ -5,15 +5,22 @@ import { getClientGqlSsr } from '../src/getClientGQL'
 
 export default async (ctx)=>{
     if (ctx.req) {
-        console.log(ctx.req.headers['user-agent'])
+        console.log(`ctx.req${1}`)
         let ua = uaParserJs(ctx.req.headers['user-agent'])
+        console.log(`ctx.req${2}`)
         ctx.store.getState().app.isMobileApp = ['mobile', 'tablet'].includes(ua.device.type)||checkMobile(ua.ua)
+        console.log(`ctx.req${3}`)
         ctx.store.getState().user.authenticated = checkAuth(ctx.req.headers.cookie)
+        console.log(`ctx.req${4}`)
         if (ctx.store.getState().user.authenticated) {
+            console.log(`ctx.req${5.1}`)
             ctx.store.getState().user.profile = await getProfile(await getClientGqlSsr(ctx.req))
+            console.log(`ctx.req${5.1}`)
         }
         else {
+            console.log(`ctx.req${5.2}`)
             ctx.store.getState().user.profile = {}
+            console.log(`ctx.req${5.2}`)
         }
     }
     ctx.store.getState().app.search = ''
